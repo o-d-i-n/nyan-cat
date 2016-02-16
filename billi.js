5c60e89006e6dc1891304fa363a1da338de39625
Divjot Singh <bogas04@gmail.com>
2016-02-16 15:16:53 +0530

    code cleanup, usage instructions

--- billi.js
+++ billi.js
@@ -1,16 +1,37 @@
 #!/usr/bin/env node
-var fs = require('fs')
-  , chalk = require('chalk')
-  , filename = process.argv[2];
-fs.readFile(filename, 'utf8', function(err, data) {
-  if (err) throw err;
-  data = data.split('\n')
-  var spaceCount = "";
-  for(var i = 0;i < String(data.length).length + 2;i++)
-    spaceCount += " ";
-  for(itr in data) {
-    if(itr == data.length - 1)
-      break;
-    console.log(chalk.grey(itr) + spaceCount.substr(0,spaceCount.length - String(itr).length) + data[itr]);
-  }
-});
+'use strict';
+
+const fs = require('fs');
+const chalk = require('chalk')
+const usage = `Usage: billi [filename]`;
+
+const purr = (filename, cb) => {
+
+  if (typeof cb === 'undefined') { cb = console.log; }
+
+  let output = '';
+
+  fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) throw err;
+
+    output = data.split('\n')
+    .map((line, number) => chalk.grey(number + 1) + '  ' + line)
+    .join('\n');
+    
+    // Always explain when you use for loop.
+    //data = data.split('\n')
+    //let spaceCount = "";
+
+    //for(let i = 0; i < String(data.length).length + 2; i++) { spaceCount += " "; }
+    //for(let itr in data) {
+      //if(itr == data.length - 1)
+        //break;
+      //output += (chalk.grey(itr) + spaceCount.substr(0,spaceCount.length - String(itr).length) + data[itr]) + (itr === data.length - 2 ? '' : '\n');
+    //}
+
+    cb(output);
+  });
+
+};
+
+if (process.argv.length < 3) { console.log(usage); } else { purr(process.argv[2]); }
