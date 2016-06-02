66b91453eb6947d6ac43a6ae5d958e587eed7514
MohitS10 <iammohitsharma@gmail.com>
2016-06-02 12:35:50 +0530

    improved readability

--- core-plugins/html.js
+++ core-plugins/html.js
@@ -4,132 +4,132 @@ const chalk = require('chalk');
 module.exports = (text) => (
   new Promise((resolve, reject) => {
 
-    let buildWord = '';
-    let buildML = '';
-    let buildTag = '';
-    let wordStart = false;
-    let styleStart = false;
-    let tagStart = false;
-    let commentStart = false;
-    let first = false;
-    let escaped = false;
+    let buildWord = ''
+    let buildML = ''
+    let buildTag = ''
+    let wordStart = false
+    let styleStart = false
+    let tagStart = false
+    let commentStart = false
+    let first = false
+    let escaped = false
     let doubleQuotesUsed = false;
 
     for (let c of text) {
       if (c === "<") {
-          tagStart = true;
-          first = true;
+          tagStart = true
+          first = true
       } else if (c === ">") {
           
           if(buildWord)
           {
-            buildTag += chalk.green(buildWord);
-            buildWord = '';
+            buildTag += chalk.green(buildWord)
+            buildWord = ''
           }
 
           if(commentStart) {
-            buildTag = chalk.stripColor(buildTag);
-            buildTag = chalk.grey(buildTag); 
+            buildTag = chalk.stripColor(buildTag)
+            buildTag = chalk.grey(buildTag)
           }
-          buildML += buildTag;
-          tagStart = false;
-          commentStart = false;
-          wordStart = false;
-          buildTag = '';
+          buildML += buildTag
+          tagStart = false
+          commentStart = false
+          wordStart = false
+          buildTag = ''
       }
 
       if(tagStart && c!= "<" && c!=">") {
 
         if (c === "\"" || c === "'") {
           if (!wordStart) {
-            buildWord += c;
-            wordStart = true;
+            buildWord += c
+            wordStart = true
             if (c === "\"") {
-              doubleQuotesUsed = true;
+              doubleQuotesUsed = true
             } else {
-              doubleQuotesUsed = false;
+              doubleQuotesUsed = false
             }
           } 
           else {
-            buildWord += c;
+            buildWord += c
             if (escaped) {
-              escaped = false;
+              escaped = false
             } else if ((c == "'" && !doubleQuotesUsed)||(c == "\"" && doubleQuotesUsed)) {
               if (styleStart) {
-                buildTag += chalk.blue(buildWord);
-                styleStart = false;
+                buildTag += chalk.blue(buildWord)
+                styleStart = false
               } else {
-              buildTag += chalk.yellow(buildWord);
+              buildTag += chalk.yellow(buildWord)
               }
-              buildWord = '';
-              wordStart = false;
+              buildWord = ''
+              wordStart = false
             }
           }
         } else if (c === "\\") {
           if (wordStart) {
-            buildWord += c;
+            buildWord += c
             if (escaped) {
-              escaped = false;
+              escaped = false
             } 
             else {
-              escaped = true;
+              escaped = true
             }
           } 
           else {
-            buildTag += c;
+            buildTag += c
            }
         } else if (c === "=") {
             if(wordStart) {
-                buildWord +=c;
+                buildWord +=c
                 if (escaped)
-                  escaped = false;
+                  escaped = false
               } else {
-                buildTag += chalk.green(buildWord);
-                buildTag += chalk.white(c);
-                wordStart=false;
+                buildTag += chalk.green(buildWord)
+                buildTag += chalk.white(c)
+                wordStart=false
                 if(buildWord==="style")
-                  styleStart=true;
-                buildWord='';
+                  styleStart=true
+                buildWord=''
               }
         } else if (c == ' ') {
           if(wordStart)
-            buildWord += c;
+            buildWord += c
           else {
             if (first) {
-              buildTag += c;
-              first = false;
-              wordStart = true;
+              buildTag += c
+              first = false
+              wordStart = true
             } else
-                buildWord += c;
-                buildTag += chalk.green(buildWord);
-                buildWord = '';
-                wordStart = false;
+                buildWord += c
+                buildTag += chalk.green(buildWord)
+                buildWord = ''
+                wordStart = false
             }
         } else if (c === "!") {
             if(first){
-              buildTag += c;
-              commentStart = true;  
+              buildTag += c
+              commentStart = true  
             } else {
-              buildWord += c;
+              buildWord += c
             }
         } else {
           if (first) {
-              buildTag += chalk.red(c);
+              buildTag += chalk.red(c)
           } else {
-            buildWord += c;
+            buildWord += c
             if (escaped) {
-              escaped = false;
+              escaped = false
             }
           }
         }
 
       }
       else{
-        buildML += chalk.magenta(c);
+        buildML += chalk.magenta(c)
       }
 
     }
 
-    resolve(buildML);
+    resolve(buildML)
   })
 );
