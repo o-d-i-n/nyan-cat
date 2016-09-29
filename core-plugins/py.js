53c4e5240e23a0df86a6176d8a387f75c07dbc12
MohitS10 <iammohitsharma@gmail.com>
2016-09-29 21:09:54 +0530

    keywords

--- core-plugins/py.js
+++ core-plugins/py.js
@@ -3,8 +3,27 @@ const chalk = require('chalk');
 
 module.exports = (text) => (
   new Promise((resolve, reject) => {
-	let parsed = ''	
-
-    resolve(parsed)
+	let buildWord = ''
+    let buildPython = ''
+    let list = ["def" , "in"]
+    for (let c of text) {
+    	let a = c.trim()
+    	if(a==='')
+    	{
+    		console.log("def" in list)
+    		if(list.indexOf(buildWord)>=0)
+    			buildPython += chalk.red(buildWord)
+    		else
+    			buildPython += buildWord
+    		buildPython += c
+    		buildWord = ''
+    	}
+    	else 
+		{   
+			buildWord += c
+    	}
+    }
+	buildPython+=buildWord
+    resolve(buildPython)
   })
 );
---
+++ test.py
@@ -0,0 +1,5 @@
+def abc() :
+	for x in range(0,10):
+		print x
+
+abc()
