f8dd9771824eaaa0b08ba5e6d2e7086979edf1fd
Mohit Sharma <iammohitsharma@gmail.com>
2016-10-09 15:33:16 +0530

    Python parser (#10)
    Python highlighter

---
+++ core-plugins/py.js
@@ -0,0 +1,142 @@
+'use strict';
+const chalk = require('chalk');
+
+module.exports = (text) => (
+  new Promise((resolve, reject) => {
+	let buildWord = ''
+    let buildPython = ''
+    let stringStart = false
+    let doubleQuotes = false
+    let commentStart = false
+    let functionName = false
+    let functionList = []
+    let keywords = [ "False", "None", "True", "and", "as", "assert", "break", "class", "continue", "def", "del", "elif", "else", "except", "finally", "for", "from", "global", "if", "import", "in", "is", "lambda", "nonlocal", "not", "or", "pass", "raise", "return", "try", "while", "with", "yield" ]
+    let builtin = [ "abs", "all", "any", "ascii", "basestring", "bin", "bool", "bytearray", "bytes", "callable", "chr", "classmethod", "cmp", "compile", "complex", "delattr", "dict", "dir", "divmod", "enumerate", "eval", "exec", "execfile", "file", "filter", "float", "format", "frozenset", "getattr", "globals", "hasattr", "hash", "help", "hex", "id", "input", "int", "isinstance", "issubclass", "iter", "len", "list", "locals", "long", "map", "max", "memoryview", "min", "next", "object", "oct", "open", "ord", "pow", "print", "property", "range", "raw_input", "reduce", "reload", "repr", "reversed", "round", "set", "setattr", "slice", "sorted", "staticmethod", "str", "sum", "super", "tuple", "type", "unichr", "unicode", "vars", "xrange", "zip", "__import__" ]
+    let bracketFlag = false
+    let bracketOpenFlag = false
+    for (let c of text) {
+    	let a = c.trim()
+        
+    	if(c==="#" && !stringStart) {
+    		buildPython += buildWord
+    		buildWord = ''
+    		buildWord += c
+    		commentStart = true
+    	}
+    	else if(commentStart) {
+    		buildWord += c
+    		if(c=='\n') {
+    			buildPython += chalk.grey(buildWord)
+    			buildWord = ''
+    			commentStart = false
+    		}
+    	}
+    	// else if(a==='' && !stringStart) {
+    	// 	if(keywords.indexOf(buildWord)>=0)
+    	// 		buildPython += chalk.red(buildWord)
+    	// 	else if(builtin.indexOf(buildWord)>=0)
+    	// 		buildPython += chalk.blue(buildWord)	
+    	// 	else
+    	// 		buildPython += buildWord
+    	// 	buildPython += c
+    	// 	buildWord = ''
+    	// }
+    	else if (c === "\"" || c === "\'") {	
+    		if(!stringStart) {
+    			stringStart = true
+    			if (c === "\"")
+    				doubleQuotes = true
+    			buildPython += buildWord
+    			buildWord = ''
+    			buildWord += c
+    		}
+    		else {
+    			if(doubleQuotes) {
+    				if (c === "\"") {
+    					buildWord += c
+    				    buildPython += chalk.yellow(buildWord)
+    				    buildWord = ''
+    				    doubleQuotes = false
+    				    stringStart = false
+    				}
+    				else
+    					buildWord += c
+    			}
+    			else {
+    				if (c === "\'") {
+    					buildWord += c
+    				    buildPython += chalk.yellow(buildWord)
+    				    buildWord = ''
+    				    stringStart = false
+    				}
+    				else
+    					buildWord += c	
+    			}
+    		}
+    	}
+        else if (builtin.indexOf(buildWord)>-1 && (c == " " || c == "(")){
+            buildPython += chalk.blue(buildWord)
+            
+            buildPython += chalk.white(c)
+            buildWord = ''
+            if ( c === "("){
+                bracketOpenFlag = true
+            }
+
+        }
+        else if(keywords.indexOf(buildWord)> -1 && (c == " " || c == "(")){
+            buildPython += chalk.red(buildWord)
+            
+            buildPython += chalk.white(c)
+            buildWord = ''
+            if ( c === "("){
+                bracketOpenFlag = true
+            }
+        }
+        else if (!isNaN(c) && buildWord === ''){
+            buildPython += chalk.cyan(c)
+            buildWord = ''
+        }
+        else if (c==='('){
+            if(builtin.indexOf(buildWord) == -1){
+                buildPython += chalk.green(buildWord);
+            }
+            
+            buildPython += chalk.white(c)
+            buildWord = ''
+            bracketOpenFlag = true
+        }
+        else if (c === ',' && bracketOpenFlag){
+            buildPython += chalk.magenta(buildWord)
+            buildWord = ''
+            buildPython += chalk.white(c)
+        } 
+        else if(c === ')'){
+            if(buildWord != ''){
+                buildPython += chalk.magenta(buildWord)
+                buildWord = ''
+                buildPython += chalk.white(c)
+                bracketOpenFlag = false
+            } else{
+                buildPython += chalk.white(c)
+                bracketOpenFlag = false
+            }
+        }
+        
+        else if(c === ':'){
+            buildPython += chalk.white(c);
+        }
+        else if(c == " " && !stringStart && !doubleQuotes && !commentStart && !bracketOpenFlag){
+            buildPython += chalk.white(buildWord)
+            buildPython += c
+            buildWord = ''
+        }
+        
+    	else {   
+			buildWord += c
+    	}
+    }
+	buildPython+=buildWord
+    resolve(buildPython)
+  })
+);
---
+++ test.py
@@ -0,0 +1,6 @@
+#test1
+def abc(test1,test2) :
+	for interval in range(0,10):
+		print "hello 'world'"
+abc(2,3)
+
